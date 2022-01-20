efe2d17fedf9f8fab8c0580802dfe31c4938ce93
Takeshi Kondo <ncukondo@gmail.com>
2022-01-21 08:30:06 +0900

    change data file to csv

--- download_r4.sh
+++ download_r4.sh
@@ -3,19 +3,20 @@ set -Ceuo pipefail
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-inputFile='r4_gsheets.config'
+inputFile=${1:-'r4_gsheets.csv'}
 
+skip_first(){ sed '1d'; }
 ensure_return(){ awk 1;}
-skip_empty(){ sed '/^$/d';}
+skip_blank(){ sed '/^$/d';}
 add_bom(){ sed -e '1s/^/\xef\xbb\xbf/';}
 
 
 cat $inputFile \
-| skip_empty   \
+| skip_first   \
+| skip_blank   \
 | ensure_return \
-| while IFS=, read title sheetid gid; do
+| while IFS=, read output sheetid gid; do
     url=https://docs.google.com/spreadsheets/d/${sheetid}/export?format=csv\&gid=${gid}
-    output=./raw/${title}.csv
     echo Downloading... $url to $output
     curl -L $url | add_bom >| $output
   done 
