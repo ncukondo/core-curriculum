3022573c3dcb5f498dec9d77abc2c920f6632d5d
Takeshi Kondo <ncukondo@gmail.com>
2022-01-31 16:33:16 +0900

    changed way of load credentials

--- download_sheets.py
+++ download_sheets.py
@@ -37,12 +37,16 @@ client_secret=f'''
 {"}"}
 '''
 
+with open("./client_secret.json","w") as f:
+    f.write(client_secret)
+
 def get_creds(scopes:list,client_secret_file:str):
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-        json.loads(client_secret),
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        "./client_secret.json",
         scopes=scopes
     )
     return credentials
+
 
 _sheet_app:gspread.Client=None
 def get_sheetapp()->gspread.Client:
