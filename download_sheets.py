69f1d9b9f334d284270339bda150fdddad97ac7b
Takeshi Kondo <ncukondo@gmail.com>
2022-01-31 17:18:49 +0900

    refactor code

--- download_sheets.py
+++ download_sheets.py
@@ -2,11 +2,6 @@ import os.path
 import os
 import json
 from dateutil import parser
-from datetime import date, time, datetime
-
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import pandas as pd
@@ -37,17 +32,12 @@ client_secret=f'''
 {"}"}
 '''
 
-'''
-with open("./client_secret.json","w") as f:
-    f.write(client_secret)
-'''
-def get_creds(scopes:list,client_secret_file:str):
+def get_creds(scopes:list):
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(
         json.loads(client_secret),
         scopes=scopes
     )
     return credentials
-
 
 _sheet_app:gspread.Client=None
 def get_sheetapp()->gspread.Client:
@@ -55,23 +45,23 @@ def get_sheetapp()->gspread.Client:
     if _sheet_app==None:
         _sheet_app=gspread.authorize(get_creds([
             'https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive'],"client_secret.json"))
+            'https://www.googleapis.com/auth/drive']))
     return _sheet_app
 
 info=pd.read_csv("./r4_gsheets_info.csv")
-for i,url in enumerate(list(info["url"])):
-    spreadsheet = get_sheetapp().open_by_url(url)
-    sheet_name=spreadsheet.title
-    modified_recorded=None
-    if str(info.at[i,"lastUpDate"])!="nan": 
-        modified_recorded=parser.parse(str(info.at[i,"lastUpDate"]))
-    else:
-        modified_recorded=parser.parse("1980-10-06 12:00Z")
-    lastUpdate=spreadsheet.lastUpdateTime
-    modified=parser.parse(lastUpdate)
-    print(f"{sheet_name} - Updated:{modified > modified_recorded}")
+try:
+    for i,url in enumerate(list(info["url"])):
+        spreadsheet = get_sheetapp().open_by_url(url)
+        sheet_name=spreadsheet.title
+        modified_recorded=None
+        if str(info.at[i,"lastUpDate"])!="nan": 
+            modified_recorded=parser.parse(str(info.at[i,"lastUpDate"]))
+        else:
+            modified_recorded=parser.parse("1980-10-06 12:00Z")
+        lastUpdate=spreadsheet.lastUpdateTime
+        modified=parser.parse(lastUpdate)
+        print(f"{sheet_name} - Updated:{modified > modified_recorded}")
 
-    try:
         if modified > modified_recorded:
             for sheet in list(spreadsheet.worksheets()):
                 dir = f"./raw/gsheets/{sheet_name}"
@@ -81,8 +71,8 @@ for i,url in enumerate(list(info["url"])):
                 print(f'saving {dir}/{sheet.title}.csv')
                 data.to_csv(f"{dir}/{sheet.title}.csv",encoding="utf_8_sig",index=False)
         info.at[i,"lastUpDate"]=lastUpdate
-    except:
-        print("APIError")
+except:
+    print("APIError")
 
 info.to_csv("./r4_gsheets_info.csv",encoding="utf_8_sig",index=False)
 
